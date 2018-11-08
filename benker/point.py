7be08e94d96e5c8ef9e06c8c9626440adc1cb6fc
Laurent LAPORTE <laurent.laporte.pro@gmail.com>
2018-11-08 18:21:49 +0100

    Add the class :class:`~benker.point.Dimension` to store cell width and height.

--- benker/point.py
+++ benker/point.py
@@ -12,19 +12,42 @@ from benker.utils import int_to_alphabet
 
 class Point(collections.namedtuple('Point', ['x', 'y'])):
     """
-    Simple *Point* tuple with *x*, *y* coordinates.
+    Coordinates of a cell in a grid: *x* is the left column, *y* if the top row.
 
     Usage:
 
     >>> from benker.point import Point
 
-    >>> p = Point(5, 3)
-    >>> p
+    >>> pt = Point(5, 3)
+    >>> pt
     Point(x=5, y=3)
-    >>> str(p)
+    >>> str(pt)
     'E3'
     """
     __slots__ = ()
 
     def __str__(self):
         return int_to_alphabet(self.x) + str(self.y)
+
+
+PT_ORIGIN = Point(1, 1)
+
+
+class Dimension(collections.namedtuple('Dimension', ['width', 'height'])):
+    """
+    Dimension of a cell: *width* is the number of columns and *height* is the number of row.
+
+    Usage:
+
+    >>> from benker.point import Dimension
+
+    >>> dim = Dimension(2, 1)
+    >>> dim
+    Dimension(width=2, height=1)
+    >>> str(dim)
+    '(2 x 1)'
+    """
+    __slots__ = ()
+
+    def __str__(self):
+        return "({width} x {height})".format(width=self.width, height=self.height)
---
+++ docs/tuto/point.rst
@@ -0,0 +1,52 @@
+.. _benker__point:
+
+.. doctest:: point
+    :hide:
+
+    >>> from benker.point import Point
+    >>> from benker.point import Dimension
+    >>> from benker.point import PT_ORIGIN
+
+
+Playing with Point and Dimension
+================================
+
+Point
+-----
+
+A *Point* is a tuple with *x*, *y* coordinates.
+It represents the top-left origin of a cell in a grid.
+
+.. doctest:: point
+
+    >>> pt = Point(4, 5)
+
+The origin of the grid is the point (1, 1).
+
+.. doctest:: point
+
+    >>> PT_ORIGIN
+    Point(x=1, y=1)
+
+We use the Excel convention to represent a *Point*:
+columns are represented by letters,
+rows are represented by numbers.
+
+    >>> print(Point(2, 5))
+    B5
+
+
+Dimension
+---------
+
+A *Dimension* is a tuple with *width*, *height* coordinates.
+It represents the width and the height of a cell in a grid.
+
+.. doctest:: point
+
+    >>> dim = Dimension(4, 5)
+
+The representation of a dimension is "(*width*, *height*):
+
+    >>> print(dim)
+    (4 x 5)
