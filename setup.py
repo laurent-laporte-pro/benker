6b0eb3109607c901d60ad8ee1b7bb1977ee5a009
Laurent LAPORTE <laurent.laporte.pro@gmail.com>
2018-11-16 09:55:09 +0100

    Update the projet configuration to use ``lxml`` and ``pytest`` and other development tools.

--- setup.py
+++ setup.py
@@ -5,6 +5,22 @@ import re
 from setuptools import find_packages
 from setuptools import setup
 
+install_requires = [
+    'lxml',
+]
+
+tests_require = [
+    'Tox',
+    'PyTest',
+    'PyTest-Cov',
+    'py',
+    'isort',
+    'check-manifest',
+    'Sphinx'
+]
+
+extras_require = {"dev": tests_require}
+
 
 def read(filename):
     filename = os.path.join(os.path.dirname(__file__), filename)
@@ -16,8 +32,6 @@ def read(filename):
 setup(
     name="Benker",
     version="0.1.0",
-    url="https://github.com/laurent-laporte-pro/benker",
-    license='MIT',
 
     author="Laurent LAPORTE",
     author_email="laurent.laporte.pro@gmail.com",
@@ -26,19 +40,41 @@ setup(
     long_description=read("README.rst"),
 
     packages=find_packages(exclude=('tests',)),
+    include_package_data=True,
+    exclude_package_data={'benker': []},
+    zip_safe=True,
 
-    install_requires=[],
+    url="https://github.com/laurent-laporte-pro/benker",
+    license='MIT',
+    platforms=['posix', 'nt'],
+    keywords='Office, Word, Excel, PowerPoint, docx, xlsx, pptx, CALS, HTML, table, converter, conversion',
 
+    install_requires=install_requires,
+    extras_require=extras_require,
+
+    # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Topic :: Text Processing',
+        'Topic :: Text Processing :: Markup',
+        'Topic :: Text Processing :: Markup :: HTML',
+        'Topic :: Text Processing :: Markup :: XML',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Manufacturing',
+        'Topic :: Printing',
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Operating System :: POSIX :: Other',
+        'Operating System :: Unix',
     ],
 )
