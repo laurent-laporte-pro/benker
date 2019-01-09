0251bd52c18416f9ff17da7ea375ba975fbb878f
Laurent LAPORTE <laurent.laporte.pro@gmail.com>
2019-01-09 21:57:07 +0100

    Changes in unit tests: improve CALS comparator for better diagnostics.

--- tests/converters/cals_comparator.py
+++ tests/converters/cals_comparator.py
@@ -1,4 +1,8 @@
 # coding: utf-8
+from __future__ import print_function
+
+import sys
+
 from lxml import etree
 
 # noinspection PyProtectedMember
@@ -17,7 +21,7 @@ class CalsComparator(object):
         exp_tables = exp_tree.xpath("//table")
         self._compare_children(src_tables, exp_tables)
 
-    def _compare_children(self, exp_children, src_children):
+    def _compare_children(self, src_children, exp_children):
         assert len(src_children) == len(exp_children)
         for src_table, exp_table in zip(src_children, exp_children):
             method_name = "compare_{tag}".format(tag=src_table.tag)
@@ -27,16 +31,26 @@ class CalsComparator(object):
 
     def _compare_elements(self, src_tree, exp_tree):
         # type: (ElementType, ElementType) -> None
-        assert src_tree.tag == exp_tree.tag
-        src_keys = sorted(src_tree.attrib.keys())
-        exp_keys = sorted(exp_tree.attrib.keys())
-        assert src_keys == exp_keys, "src: {0!r}, exp: {1!r}".format(src_keys, exp_keys)
-        src_attrs = sorted(src_tree.attrib.items())
-        exp_attrs = sorted(exp_tree.attrib.items())
-        assert src_attrs == exp_attrs, "src: {0!r}, exp: {1!r}".format(src_attrs, exp_attrs)
-        src_children = list(src_tree)
-        exp_children = list(exp_tree)
-        self._compare_children(exp_children, src_children)
+        try:
+            assert src_tree.tag == exp_tree.tag
+            src_keys = sorted(src_tree.attrib.keys())
+            exp_keys = sorted(exp_tree.attrib.keys())
+            assert src_keys == exp_keys, "src: {0!r}, exp: {1!r}".format(src_keys, exp_keys)
+            src_attrs = sorted(src_tree.attrib.items())
+            exp_attrs = sorted(exp_tree.attrib.items())
+            assert src_attrs == exp_attrs, "src: {0!r}, exp: {1!r}".format(src_attrs, exp_attrs)
+        except AssertionError:
+            tree = exp_tree.getroottree()
+            src_proto = etree.Element(src_tree.tag, attrib=src_tree.attrib)
+            exp_proto = etree.Element(exp_tree.tag, attrib=exp_tree.attrib)
+            print("FAIL: invalid xpath: {0}".format(tree.getpath(exp_tree)), file=sys.stderr)
+            print("source:   " + etree.tounicode(src_proto, with_tail=False), file=sys.stderr)
+            print("expected: " + etree.tounicode(exp_proto, with_tail=False), file=sys.stderr)
+            raise
+        else:
+            src_children = list(src_tree)
+            exp_children = list(exp_tree)
+            self._compare_children(exp_children, src_children)
 
     compare_table = _compare_elements
     compare_tgroup = _compare_elements
