2016165232e8f3ad29ffeb3a1f8388cd167a8cd4
Laurent LAPORTE <laurent.laporte.pro@gmail.com>
2019-09-12 12:39:20 +0200

    Prepare next beta version v0.5.0b2

--- benker/__init__.py
+++ benker/__init__.py
@@ -1,5 +1,5 @@
 """Benker - Easily convert your CALS, HTML, Formex4, Office Open XML (docx) tables from one format to another."""
 
-__version__ = "0.5.0b1"
+__version__ = "0.5.0b2"
 __author__ = "Laurent LAPORTE <laurent.laporte.pro@gmail.com>"
 __all__ = []
