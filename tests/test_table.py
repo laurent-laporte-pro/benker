37635c5ae87a72c1d943ee33333f6f15b670ed27
Laurent LAPORTE <laurent.laporte.pro@gmail.com>
2019-09-16 11:13:38 +0200

    Add unit tests to check cell spanning.

--- tests/test_table.py
+++ tests/test_table.py
@@ -5,6 +5,34 @@ import textwrap
 from benker.table import Table
 
 
+def test_colspan():
+    table = Table()
+    table.cols[1].insert_cell("one")
+    table.cols[1].insert_cell("spanned", width=2)
+    table.cols[2].insert_cell("two")
+    text = str(table)
+    assert text == textwrap.dedent("""\
+    +-----------+-----------+
+    |    one    |    two    |
+    +-----------------------+
+    |  spanned              |
+    +-----------------------+""")
+
+
+def test_rowspan():
+    table = Table()
+    table.rows[1].insert_cell("one")
+    table.rows[1].insert_cell("spanned", height=2)
+    table.rows[2].insert_cell("two")
+    text = str(table)
+    assert text == textwrap.dedent("""\
+    +-----------+-----------+
+    |    one    |  spanned  |
+    +-----------|           |
+    |    two    |           |
+    +-----------+-----------+""")
+
+
 def test_fill_missing_cells():
     table = Table()
     table.rows[1].insert_cell("one")
