fd6e203293e272cd459493e57d0ee4d9df29de1d
Laurent LAPORTE <laurent.laporte.pro@gmail.com>
2019-09-17 15:53:41 +0200

    Add unit tests for the function :func:`~benker.converters.formex2cals.convert_formex2cals`.

---
+++ tests/converters/test_convert_formex2cals.py
@@ -0,0 +1,52 @@
+# coding: utf-8
+from __future__ import print_function
+
+import shutil
+
+import py.path  # type hints
+import pytest
+import xmldiff.main
+from lxml import etree
+
+from benker.converters.formex2cals import convert_formex2cals
+
+from tests.resources import RESOURCES_DIR
+
+
+@pytest.mark.parametrize(
+    'input_name, expected_name',
+    [
+        # ("formex/tbl_small_table.xml", "formex2cals/tbl_small_table.xml"),
+        # ("formex/tbl_sample.xml", "formex2cals/tbl_sample.xml"),
+        ("formex/tbl_sample_cals.xml", "formex2cals/tbl_sample_cals.xml")
+    ],
+)
+def test_convert_formex2cals(input_name, expected_name, tmpdir):
+    # type: (str, str, py.path.local) -> None
+    src_xml = RESOURCES_DIR.join(input_name)  # type: py.path.local
+    dst_xml = tmpdir.join(src_xml.basename)
+    convert_formex2cals(
+        str(src_xml),
+        str(dst_xml),
+        width_unit='mm',
+        cals_prefix="cals",
+        cals_ns="https://jouve.com/schemas/opue/doj/formex-cals",
+    )
+
+    # - Compare with expected
+    xml_parser = etree.XMLParser(remove_blank_text=True)
+    expected_xml = RESOURCES_DIR.join(expected_name)  # type: py.path.local
+    if expected_xml.exists():
+        expected_tree = etree.parse(str(expected_xml), parser=xml_parser)
+        expected_elements = expected_tree.xpath("//table")
+        dst_tree = etree.parse(str(dst_xml), parser=xml_parser)
+        dst_elements = dst_tree.xpath("//table")
+        assert len(expected_elements) == len(dst_elements)
+
+        for dst_elem, expected_elem in zip(dst_elements, expected_elements):
+            diff_list = xmldiff.main.diff_trees(dst_elem, expected_elem)
+            assert not diff_list
+    else:
+        # missing resource: create it
+        shutil.copy(str(dst_xml), str(expected_xml))
+        print("You should check and commit the file: '{}'".format(expected_xml))
