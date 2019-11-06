c54d52bd3913cc862bd6c13e27474af0df2a847e
Laurent LAPORTE <laurent.laporte.pro@gmail.com>
2019-11-06 22:04:28 +0100

    Add unit tests for RGB colors.

--- benker/colors/rgb.py
+++ benker/colors/rgb.py
@@ -18,11 +18,11 @@ def parse_hex8(text, scale=255):
     if mo:
         r, g, b, a = mo.group("r", "g", "b", "a")
         k = scale / 255.0
-        r = int(r, 16) * k
-        g = int(g, 16) * k
-        b = int(b, 16) * k
+        r = round(int(r, 16) * k)
+        g = round(int(g, 16) * k)
+        b = round(int(b, 16) * k)
         if a:
-            a = int(a, 16) * k
+            a = int(a, 16) / 255.0
             return r, g, b, a
         else:
             return r, g, b, None
@@ -38,7 +38,7 @@ def format_hex8(r, g, b, a=None, scale=255):
     if a is None:
         fmt = "#{r:02x}{g:02x}{b:02x}"
     else:
-        a = round(a / k)
+        a = round(a * 255.0)
         fmt = "#{r:02x}{g:02x}{b:02x}{a:02x}"
     return fmt.format(r=r, g=g, b=b, a=a)
 
@@ -48,11 +48,11 @@ def format_hex8_upper(r, g, b, a=None, scale=255):
 
 
 def format_hex6(r, g, b, a=None, scale=255):
-    return format_hex8(r, g, b, a=a, scale=scale)[:8]
+    return format_hex8(r, g, b, a=a, scale=scale)[:7]
 
 
 def format_hex6_upper(r, g, b, a=None, scale=255):
-    return format_hex8(r, g, b, a=a, scale=scale)[:8].upper()
+    return format_hex8(r, g, b, a=a, scale=scale)[:7].upper()
 
 
 _match_hex4 = re.compile(r"^#(?P<r>[0-9a-f])(?P<g>[0-9a-f])(?P<b>[0-9a-f])(?P<a>[0-9a-f])?$", flags=re.I).match
@@ -63,11 +63,11 @@ def parse_hex4(text, scale=255):
     if mo:
         r, g, b, a = mo.group("r", "g", "b", "a")
         k = scale / 255.0
-        r = int(r * 2, 16) * k
-        g = int(g * 2, 16) * k
-        b = int(b * 2, 16) * k
+        r = round(int(r * 2, 16) * k)
+        g = round(int(g * 2, 16) * k)
+        b = round(int(b * 2, 16) * k)
         if a:
-            a = int(a * 2, 16) * k
+            a = int(a * 2, 16) / 255.0
             return r, g, b, a
         else:
             return r, g, b, None
@@ -83,7 +83,7 @@ def format_hex4(r, g, b, a=None, scale=255):
     if a is None:
         fmt = "#{r:01x}{g:01x}{b:01x}"
     else:
-        a = round(a / k / 17)
+        a = round(a * 255.0 / 17)
         fmt = "#{r:01x}{g:01x}{b:01x}{a:01x}"
     return fmt.format(r=r, g=g, b=b, a=a)
 
@@ -148,4 +148,16 @@ def format_rgba(r, g, b, a=None, scale=255):
         fmt = "rgb({r:g}, {g:g}, {b:g})"
     else:
         fmt = "rgba({r:g}, {g:g}, {b:g}, {a:g})"
+    return fmt.format(r=r, g=g, b=b, a=a)
+
+
+def format_rgba_percent(r, g, b, a=None, scale=255):
+    r = round(r / scale * 100.0, 2)
+    g = round(g / scale * 100.0, 2)
+    b = round(b / scale * 100.0, 2)
+    if a is None:
+        fmt = "rgb({r:.5g}%, {g:.5g}%, {b:.5g}%)"
+    else:
+        a = round(a * 100.0, 2)
+        fmt = "rgba({r:.5g}%, {g:.5g}%, {b:.5g}%, {a:.5g}%)"
     return fmt.format(r=r, g=g, b=b, a=a)
--- tests/colors/test_rgb.py
+++ tests/colors/test_rgb.py
@@ -1,8 +1,110 @@
 # coding: utf-8
 import pytest
 
+from benker.colors.rgb import format_hex3
+from benker.colors.rgb import format_hex3_upper
+from benker.colors.rgb import format_hex4
+from benker.colors.rgb import format_hex4_upper
+from benker.colors.rgb import format_hex6
+from benker.colors.rgb import format_hex6_upper
+from benker.colors.rgb import format_hex8
+from benker.colors.rgb import format_hex8_upper
 from benker.colors.rgb import format_rgba
+from benker.colors.rgb import format_rgba_percent
+from benker.colors.rgb import parse_hex4
+from benker.colors.rgb import parse_hex8
 from benker.colors.rgb import parse_rgba
+
+
+@pytest.mark.parametrize(
+    "text, expected",
+    [
+        ("#00000000", (0, 0, 0, 0)),
+        ("#000000", (0, 0, 0, None)),
+        ("#ffFFffFF", (0xFF, 0xFF, 0xFF, 1)),
+        ("#FF8040C0", (0xFF, 0x80, 0x40, 0xC0 / 255.0)),
+    ],
+)
+def test_parse_hex8(text, expected):
+    r, g, b, a = parse_hex8(text)
+    assert (r, g, b, a) == expected
+
+
+def test_parse_hex8__range():
+    for i in range(256):
+        text = "#{:02x}0000".format(i)
+        r, g, b, a = parse_hex8(text)
+        assert (r, g, b, a) == (i, 0, 0, None)
+        text = "#00{:02x}00".format(i)
+        r, g, b, a = parse_hex8(text)
+        assert (r, g, b, a) == (0, i, 0, None)
+        text = "#0000{:02x}".format(i)
+        r, g, b, a = parse_hex8(text)
+        assert (r, g, b, a) == (0, 0, i, None)
+
+
+@pytest.mark.parametrize(
+    "rgba, expected",
+    [
+        ((0, 0, 0, None), "#000000"),
+        ((0, 0, 0, 0), "#00000000"),
+        ((255, 255, 255, 1), "#ffffffff"),
+        ((255, 127.5, 63.75, 0.75), "#ff8040bf"),
+        ((123, 234, 17, 1 / 3.0), "#7bea1155"),
+    ],
+)
+def test_format_hex8(rgba, expected):
+    text = format_hex8(*rgba)
+    assert text == expected
+    assert len(text) == 1 + 8 or len(text) == 1 + 6
+    text = format_hex8_upper(*rgba)
+    assert text == expected.upper()
+    assert len(text) == 1 + 8 or len(text) == 1 + 6
+    text = format_hex6(*rgba)
+    assert text == expected[:7]
+    assert len(text) == 1 + 6
+    text = format_hex6_upper(*rgba)
+    assert text == expected[:7].upper()
+    assert len(text) == 1 + 6
+
+
+@pytest.mark.parametrize(
+    "text, expected",
+    [
+        ("#0000", (0, 0, 0, 0)),
+        ("#000", (0, 0, 0, None)),
+        ("#fFfF", (0xFF, 0xFF, 0xFF, 0xFF / 255.0)),
+        ("#F84C", (0xFF, 0x88, 0x44, 0xCC / 255.0)),
+    ],
+)
+def test_parse_hex4(text, expected):
+    r, g, b, a = parse_hex4(text)
+    assert (r, g, b, a) == expected
+
+
+@pytest.mark.parametrize(
+    "rgba, expected",
+    [
+        ((0, 0, 0, None), "#000"),
+        ((0, 0, 0, 0), "#0000"),
+        ((0xFF, 0xFF, 0xFF, 1), "#ffff"),
+        ((0xFF, 0x88, 0x44, 0.75), "#f84b"),
+        ((123, 234, 17, 1 / 3.0), "#7e15"),
+    ],
+)
+def test_format_hex4(rgba, expected):
+    text = format_hex4(*rgba)
+    assert text == expected
+    assert len(text) == 1 + 4 or len(text) == 1 + 3
+    text = format_hex4_upper(*rgba)
+    assert text == expected.upper()
+    assert len(text) == 1 + 4 or len(text) == 1 + 3
+    text = format_hex3(*rgba)
+    assert text == expected[:4]
+    assert len(text) == 1 + 3
+    text = format_hex3_upper(*rgba)
+    assert text == expected[:4].upper()
+    assert len(text) == 1 + 3
 
 
 @pytest.mark.parametrize(
@@ -27,8 +129,24 @@ def test_parse_rgba(text, expected):
         ((0, 0, 0, 0), "rgba(0, 0, 0, 0)"),
         ((255, 255, 255, 1), "rgba(255, 255, 255, 1)"),
         ((255, 127.5, 63.75, 0.75), "rgba(255, 128, 64, 0.75)"),
+        ((123, 234, 17, 1 / 3.0), "rgba(123, 234, 17, 0.333333)"),
     ],
 )
 def test_format_rgba(rgba, expected):
     text = format_rgba(*rgba)
+    assert text == expected
+
+
+@pytest.mark.parametrize(
+    "rgba, expected",
+    [
+        ((0, 0, 0, None), "rgb(0%, 0%, 0%)"),
+        ((0, 0, 0, 0), "rgba(0%, 0%, 0%, 0%)"),
+        ((255, 255, 255, 1), "rgba(100%, 100%, 100%, 100%)"),
+        ((255, 127.5, 63.75, 0.75), "rgba(100%, 50%, 25%, 75%)"),
+        ((123, 234, 17, 1 / 3.0), "rgba(48.24%, 91.76%, 6.67%, 33.33%)"),
+    ],
+)
+def test_format_rgba_percent(rgba, expected):
+    text = format_rgba_percent(*rgba)
     assert text == expected
