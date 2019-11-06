2beb127ae425e2093452c69ef5b3ff48b28d044c
Laurent LAPORTE <laurent.laporte.pro@gmail.com>
2019-11-06 22:37:52 +0100

    Remove empty tests.

--- tests/test_rgb_color.py
+++ tests/test_rgb_color.py
@@ -2,10 +2,6 @@
 from benker.colors import RGBColor
 
 
-def test_parse():
-    assert 0
-
-
 def test_parse_rgba():
     # AliceBlue
     color = RGBColor.from_string("rgb(240, 248, 255)")
@@ -15,31 +11,3 @@ def test_parse_rgba():
     assert color.a is None
     assert color.name is None
     assert str(color) == "rgb(240, 248, 255)"
-
-
-def test_parse_hsla():
-    assert 0
-
-
-def test_parse_cmyk():
-    assert 0
-
-
-def test_parse_hex8():
-    assert 0
-
-
-def test_parse_hex4():
-    assert 0
-
-
-def test_parse_name():
-    assert 0
-
-
-def test_parse_percent_name():
-    assert 0
-
-
-def test_parse_other_name():
-    assert 0
