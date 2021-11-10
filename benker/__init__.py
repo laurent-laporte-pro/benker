f60b303e4f5497702f87c2a2a7a0ca0553a2d946
Laurent LAPORTE <laurent.laporte.pro@gmail.com>
2021-11-10 23:05:54 +0100

    New bug fix release v0.4.4 (2021-11-10)

--- benker/__init__.py
+++ benker/__init__.py
@@ -1,5 +1,6 @@
-"""Benker - Organize your data in a spatial grid system for CALS, HTML, Formex4, Office Open XML tables conversion"""
+"""Organize your data in a spatial grid system for CALS, HTML, Formex4, Office Open XML tables conversion"""
 
 __version__ = "0.4.4"
 __author__ = "Laurent LAPORTE <laurent.laporte.pro@gmail.com>"
-__all__ = []
+__date__ = "2021-11-10"
+__credit__ = "(c) 2018-2021 Laurent LAPORTE"
